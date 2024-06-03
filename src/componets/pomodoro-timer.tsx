0c704627d466beff38be96dc72ace28055fddbdb
Isaias Santana dos Santos <isaiassantanadossantos@hotmail.com>
2024-06-03 20:43:53 -0300

    fix: CSS configuration

--- src/componets/pomodoro-timer.tsx
+++ src/componets/pomodoro-timer.tsx
@@ -4,10 +4,13 @@ import { Button } from "./button";
 import { Timer } from "./timer";
 
 interface Props {
-  defaultPomodoroTime: number;
+  pomodoroTime: number;
+  shortRestTime: number;
+  longRestTime: number;
+  cycles: number;
 }
 export function PomodoroTimer(props: Props): JSX.Element {
-  const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
+  const [mainTime, setMainTime] = React.useState(props.pomodoroTime);
 
   useInterval(() => {
     setMainTime(mainTime - 1);
@@ -17,7 +20,20 @@ export function PomodoroTimer(props: Props): JSX.Element {
     <div className="pomodoro">
       <h2>You are: working</h2>
       <Timer mainTimer={mainTime} />
-      <Button text="Teste" onClick={() => console.log("Clicou!")} />
+
+      <div className="controls">
+        <Button text="Teste" onClick={() => console.log("Clicou!")} />
+        <Button text="Teste" onClick={() => console.log("Clicou!")} />
+        <Button text="Teste" onClick={() => console.log("Clicou!")} />
+      </div>
+
+      <div className="details">
+        <p>Testando: dknkdnend dedd dewmd dwe </p>
+        <p>Testando: dknkdnend dedd dewmd dwe </p>
+        <p>Testando: dknkdnend dedd dewmd dwe </p>
+        <p>Testando: dknkdnend dedd dewmd dwe </p>
+        <p>Testando: dknkdnend dedd dewmd dwe </p>
+      </div>
     </div>
   );
 }
